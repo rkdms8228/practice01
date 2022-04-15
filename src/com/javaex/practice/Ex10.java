879f4ebbf8ae8ee590f070dcaa241c6d02aed46d
가은 <user@192.168.45.72>
2022-04-15 13:51:29 +0900

    2022-04-15 재수정

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -7,8 +7,8 @@ public class Ex10 {
 		/*
 		int x;
 		System.out.println(x);
-		//변수x의 값을 설정하지 않아서(선언하지 않아서)
-		  아래 System.out.println 메소드에서 값을 출력할 수 없음
+		//변수x의 값을 초기화하지(선언하지/설정하지) 않아서
+		  아래 System.out.println 메소드에서 출력할 수 없음
 		*/
 	}
 
